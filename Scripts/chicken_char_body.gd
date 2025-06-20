18f508641e9a347008d691c48beaf1ecbf327c68
Shreyash Gavit <ssg1692@gmail.com>
2025-06-20 12:00:51 +0530

    working on Enemy chicken ai

--- Scripts/chicken_char_body.gd
+++ Scripts/chicken_char_body.gd
@@ -1,19 +1,32 @@
 extends CharacterBody3D
 
 var player = null
+var Target: Vector3
+var direction: Vector3
 
-@export var speed = 4.0
+@export var speed:float = 4.0
+@export var escape_distance: float = 10.0
 
 @export var player_path: NodePath
 @export var agent_nav: NavigationAgent3D
 
+
 func _ready() -> void:
 	player = get_node(player_path)
+
+func _time_to_flee() -> void:
+	var playerPos = player.global_transform.origin
+	direction = global_transform.origin.direction_to(playerPos)
+	var cTarget = global_transform.origin - direction * escape_distance
+	cTarget.y = global_transform.origin.y
+	Target = cTarget
+
 func _process(delta: float) -> void:
 	velocity = Vector3.ZERO
 	#Navigation
-	agent_nav.set_target_position(player.global_transform.origin)
+	_time_to_flee()
+	agent_nav.set_target_position(Target)
 	var next_nav_point = agent_nav.get_next_path_position()
 	velocity = (next_nav_point - global_transform.origin).normalized() * speed
-	look_at(Vector3(player.global_transform.origin.x, global_position.y, player.global_transform.origin.z), Vector3.UP)
+	look_at(direction, Vector3.UP)
 	move_and_slide()
